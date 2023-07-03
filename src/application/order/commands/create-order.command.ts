65252ac478f5b360827ced9122ddf4e469e701c9
Willian Kazahaya <williankaza@hotmail.com>
2023-07-03 20:58:05 -0300

    Update Order commands

--- src/application/order/commands/create-order.command.ts
+++ src/application/order/commands/create-order.command.ts
@@ -1,1 +1,11 @@
-export interface CreateOrderCommand {}
+import { ICommand } from "../../../domain/base/interfaces"
+import { Customer } from "../../../domain/customer/entity/customer"
+
+export interface CreateOrderCommand extends ICommand{
+	customerId?: string,
+	customer?: Customer,
+	item?: Array<{
+		product: string,
+		quantity: number
+	}>
+}
