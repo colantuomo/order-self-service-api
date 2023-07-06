32f280b06f5361d1b985dc091d5c88cd814d80fb
Willian Yoshiaki Kazahaya <williankaza@hotmail.com>
2023-07-06 02:12:09 -0300

    Identation
    - Change Identation from 2 Spaces for 4 Tabs

--- src/application/product/product.reponse.ts
+++ src/application/product/product.reponse.ts
@@ -3,21 +3,21 @@ import { Product } from "../../domain/product/entity/product";
 
 export class ProductResponse implements IResponse<Product | Product[]> {
 
-  private _data: any;
-  constructor(data: any) {
-    this._data = data;
-  }
+	private _data: any;
+	constructor(data: any) {
+		this._data = data;
+	}
 
-  public format(data: Product | any) {
-    if (Array.isArray(data)) {
-      return data.map(({ id, name, price, category, description }) => new Product(id, name, price, category, description))
-    }
-    const { id, name, price, category, description } = data;
-    return new Product(id, name, price, category, description);
-  }
+	public format(data: Product | any) {
+		if (Array.isArray(data)) {
+			return data.map(({ id, name, price, category, description }) => new Product(id, name, price, category, description))
+		}
+		const { id, name, price, category, description } = data;
+		return new Product(id, name, price, category, description);
+	}
 
-  public get data() {
-    return this.format(this._data);
-  }
+	public get data() {
+		return this.format(this._data);
+	}
 
 }
