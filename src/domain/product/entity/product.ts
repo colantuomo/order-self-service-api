b266206e1cdae2e6047afa31185a61ac9365ebca
colantuomo <pcolantuomo@gmail.com>
2023-07-06 22:41:18 -0300

    feat/fix: healthcheck and adding exception

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,4 @@
 import { IEntity } from '../../base/interfaces';
-import { Order } from '../../order/entity/order';
 import { ProductCategories } from '../enums';
 
 export class Product implements IEntity {
@@ -9,14 +8,12 @@ export class Product implements IEntity {
 	price: number;
 	description: string | null | undefined;
 	category: ProductCategories
-	orders: Order[];
 
 	constructor(id: string, name: string, price: number, category: ProductCategories, description?: string | null) {
 		this.name = name;
 		this.price = price;
 		this.description = description;
-		this.category = category
-		this.orders = [];
+		this.category = category;
 		this.id = id;
 	}
 }
